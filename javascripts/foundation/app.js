3a02a0fe9f9ff0244d7e8f103915274474978a93
3Easy <hello@3easy.org>
2012-11-16 23:48:21 +1100

    And About Time Too

---
+++ javascripts/foundation/app.js
@@ -0,0 +1,67 @@
+;(function ($, window, undefined) {
+  'use strict';
+
+  var $doc = $(document),
+      Modernizr = window.Modernizr;
+
+  $(document).ready(function() {
+    $.fn.foundationAlerts           ? $doc.foundationAlerts() : null;
+    $.fn.foundationButtons          ? $doc.foundationButtons() : null;
+    $.fn.foundationAccordion        ? $doc.foundationAccordion() : null;
+    $.fn.foundationNavigation       ? $doc.foundationNavigation() : null;
+    $.fn.foundationTopBar           ? $doc.foundationTopBar() : null;
+    $.fn.foundationCustomForms      ? $doc.foundationCustomForms() : null;
+    $.fn.foundationMediaQueryViewer ? $doc.foundationMediaQueryViewer() : null;
+    $.fn.foundationTabs             ? $doc.foundationTabs({callback : $.foundation.customForms.appendCustomMarkup}) : null;
+    $.fn.foundationTooltips         ? $doc.foundationTooltips() : null;
+    $.fn.foundationMagellan         ? $doc.foundationMagellan() : null;
+    $.fn.foundationClearing         ? $doc.foundationClearing() : null;
+
+    $('input, textarea').placeholder();
+  });
+
+  // UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
+  // $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'both'});
+  // $('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'both'});
+  // $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'both'});
+  // $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'both'});
+
+  // Hide address bar on mobile devices (except if #hash present, so we don't mess up deep linking).
+  if (Modernizr.touch && !window.location.hash) {
+    $(window).load(function () {
+      setTimeout(function () {
+        window.scrollTo(0, 1);
+      }, 0);
+    });
+  }
+  
+  // App Click
+  $('.app').click(function(){
+	  $(this).toggleClass('off');
+  });
+    
+  // Light Selected
+  $('.light').click(function(){
+	  $('.light').removeClass('selected');
+	  $(this).addClass('selected');
+  });
+  
+  // /* Ajax */ Apps
+  $(".app > a").click(function(){
+  	var appid = $(this).attr('id');
+/*   	alert("/apps/" + appid + "/" + appid + ".html"); */
+/*
+	  $.ajax({
+		  url: "/apps/" + app + "/" + app + ".html",
+		  cache: false,
+		  success: function(html){
+		    $("body").append(html);
+		  }
+		});
+*/
+	  $("#" + appid + "-app").reveal();
+  });
+    
+  // Last One Out, Turn Out The Lights :)
+
+})(jQuery, this);

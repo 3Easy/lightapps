7ed1d5a2b038f6f64782620dc6e327b51e1c2b19
Mark Pesce <mpesce@juno.(none)>
2012-11-22 17:39:44 +1100

    Fixes to the colorwheel so it fits our new object format

--- apps/colorwheel/colorwheel.js
+++ apps/colorwheel/colorwheel.js
@@ -1,56 +1,52 @@
+// Main entry function
 
-
-window.addEventListener('load', eventWindowLoaded, false);	
-
-function eventWindowLoaded() {
-	canvasApp();
-	
-}
-
-function canvasSupport () {
- 	return Modernizr.canvas;
-}
-
-function canvasApp() {
+function colorwheel() {
 	
 	console.log("Colorwheeeeeeel")
 
-	if (!canvasSupport()) {
-			 return;
- 	} else {
-	    var theCanvas = document.getElementById('canvas');
-	    var context = theCanvas.getContext('2d');
-	    
-	    var thePicked = document.getElementById('picked');
-	    var pickcontext = thePicked.getContext('2d');
-	    
-	    var theComplement = document.getElementById('complement');
-	    var compcontext = theComplement.getContext('2d');
-	}
+	this.theCanvas = document.getElementById('canvas');
+	this.context = this.theCanvas.getContext('2d');
 	
-	var mouseX;
-	var mouseY;
-	var compR;
-	var compG;
-	var compB;
+	this.thePicked = document.getElementById('picked');
+	this.pickcontext = this.thePicked.getContext('2d');
 	
-	var tileSheet=new Image();
-	tileSheet.addEventListener('load', eventSheetLoaded , false);
-	tileSheet.src="http://lightapps/images/apps/app-colorwheel.png";
+	this.theComplement = document.getElementById('complement');
+	this.compcontext = this.theComplement.getContext('2d');
 	
-	//var imageData=context.createImageData(32,32);
+	this.mouseX = 0;
+	this.mouseY = 0;
+	this.compR = 0;
+	this.compG = 0;
+	this.compB = 0;
+	
+	this.tileSheet=new Image();
+	this.tileSheet.addEventListener('load', this.eventSheetLoaded , false);
+	this.tileSheet.src="apps/colorwheel/color_wheel.png";
+	//console.log(this.tileSheet.src);
+	
+	this.appStart = appStart;
+	this.appQuit = appQuit;
+	this.drawTileSheet = drawTileSheet;
+	this.fillpick = fillpick;
+	this.fillcomp = fillcomp;
+	this.onMouseMove = onMouseMove;
+	this.onMouseClick = onMouseClick;
+	this.onCompClick = onCompClick;
+	this.eventSheetLoaded = eventSheetLoaded;
 	
 	function eventSheetLoaded() {
-		startUp();
-		console.log('We’re loaded!')
+		console.log("loadededed");
 	}
-
-	function startUp() {
-		drawTileSheet();
+		
+	function appStart() {
+		this.drawTileSheet();
+	}
+	
+	function appQuit() {
 	}
 	
 	function drawTileSheet() {
-		context.drawImage(tileSheet, 0, 0);
+		this.context.drawImage(this.tileSheet, 0, 0);
 		
 	}
 	
@@ -67,14 +63,14 @@ function canvasApp() {
 		if (bluestr.length < 2) {
 			bluestr = "0" + bluestr;
 		}
-		pickcontext.fillStyle="#" + redstr + greenstr + bluestr;
-		pickcontext.fillRect(0,0,350,100);
+		this.pickcontext.fillStyle="#" + redstr + greenstr + bluestr;
+		this.pickcontext.fillRect(0,0,350,100);
 	}
 	
 	function fillcomp(red, green, blue){
-		compR = red = 255 - red;
-		compG = green = 255 - green;
-		compB = blue = 255 - blue;
+		this.compR = red = 255 - red;
+		this.compG = green = 255 - green;
+		this.compB = blue = 255 - blue;
 		//console.log(red, green, blue)
 		redstr = red.toString(16);
 		//console.log(redstr);
@@ -92,67 +88,44 @@ function canvasApp() {
 		}
 		fs = "#" + redstr + greenstr + bluestr;
 		//console.log(fs);
-		compcontext.fillStyle=fs;
+		this.compcontext.fillStyle=fs;
 		//console.log(compcontext.fillStyle);
-		compcontext.fillRect(0,0,350,100);			
+		this.compcontext.fillRect(0,0,350,100);			
 	}	
 	
 	function onMouseMove(e) {
-		mouseX=e.clientX-theCanvas.offsetLeft;
-		mouseY=e.clientY-theCanvas.offsetTop;
+		theApp.mouseX=e.clientX-theApp.theCanvas.offsetLeft;
+		theApp.mouseY=e.clientY-theApp.theCanvas.offsetTop;
 		//console.log(mouseX, mouseY);
 	}
 	
 	function onMouseClick(e) {
-		console.log("click: " + mouseX + "," + mouseY);
-		imageData=context.getImageData(mouseX,mouseY,1,1);
+		console.log("click: " + theApp.mouseX + "," + theApp.mouseY);
+		imageData=theApp.context.getImageData(theApp.mouseX,theApp.mouseY,1,1);
 		var red = (imageData.data[0] >> 1) | 0x80;
 		var green = (imageData.data[1] >> 1) | 0x80;
 		var blue = (imageData.data[2] >> 1) | 0x80;
-		/*console.log("imageData length: " + imageData.data.length);
-		for (i=0; i< imageData.data.length; i++){
-			console.log("data: " + imageData.data[i]);
-		}*/
-		console.log("color (" + red.toString(16) + ", " + green.toString(16) + ", " + blue.toString(16) + ")")
+		console.log("color (" + red.toString(16) + ", " + green.toString(16) + ", " + blue.toString(16) + ")");
 		
-		// This is where we'll call ajaxcolor
-		redstr = red.toString(16);
-		greenstr = green.toString(16);
-		bluestr = blue.toString(16);
-		ajaxstr = "0x" +  greenstr + redstr + bluestr;
-		console.log("ajaxstr: " + ajaxstr);
-		fillpick(imageData.data[0], imageData.data[1], imageData.data[2]);
-		fillcomp(imageData.data[0], imageData.data[1], imageData.data[2]);
-		jQuery.ajax({
-			url: '/cgi-bin/ajaxcolor', 
-			data: {color: ajaxstr}
-		});
-
+		theApp.fillpick(imageData.data[0], imageData.data[1], imageData.data[2]);
+		theApp.fillcomp(imageData.data[0], imageData.data[1], imageData.data[2]);
+		currentLight.setlamp(red, green, blue);
 	}
 
 	function onCompClick(e) {
-		console.log("click: " + mouseX + "," + mouseY);
-		imageData=compcontext.getImageData(mouseX,mouseY,1,1);
+		console.log("click: " + theApp.mouseX + "," + theApp.mouseY);
+		imageData=theApp.compcontext.getImageData(theApp.mouseX,theApp.mouseY,1,1);
 		var red = (compR >> 1) | 0x80;
 		var green = (compG >> 1) | 0x80;
 		var blue = (compB >> 1) | 0x80;
-		console.log("complement color (" + red.toString(16) + ", " + green.toString(16) + ", " + blue.toString(16) + ")")
+		console.log("complement color (" + red.toString(16) + ", " + green.toString(16) + ", " + blue.toString(16) + ")");
+		currentLight.setlamp(red, green, blue);
 		
-		// This is where we'll call ajaxcolor
-		redstr = red.toString(16);
-		greenstr = green.toString(16);
-		bluestr = blue.toString(16);
-		ajaxstr = "0x" +  greenstr + redstr + bluestr;
-		console.log("ajaxstr: " + ajaxstr);
-		jQuery.ajax({
-			url: '/cgi-bin/ajaxcolor', 
-			data: {color: ajaxstr}
-		});	
 	}
 	
-	theCanvas.addEventListener("mousemove", onMouseMove, false);
-	theCanvas.addEventListener("click", onMouseClick, false);
-	theComplement.addEventListener("click", onCompClick, false);
+	this.theCanvas.addEventListener("mousemove", this.onMouseMove, false);
+	this.theCanvas.addEventListener("click", this.onMouseClick, false);
+	this.theComplement.addEventListener("click", this.onCompClick, false);
 	
 }
 

d8dbf67ed6be2aa8245ebb52cc7a433bacae5f6d
mpesce <mpesce@gmail.com>
2012-11-25 13:26:12 +1100

    Fixes for the photosampler

--- apps/photograb/photograb.js
+++ apps/photograb/photograb.js
@@ -1,34 +1,35 @@
 // photograb.js - samples a photo, and sends the sampled color to a Light
 //
 
+var photograb_img = null;
+
 // Every app has an object
 function photograb() {
 
 	function appStart() {
+		console.log("photograb.appStart");
 		sampApp();
 	}
 	
 	function appQuit() {
-	
+		console.log("photograb.appQuit");
 	}
 		
-	this.img = null;
 	this.lastTouch = null;
 	this.appStart = appStart;
 	this.appQuit = appQuit;
-	this.imageLoaded = imageLoaded;
 	
 	this.mouseX = 0;
 	this.mouseY = 0;
 			
-	this.theCanvas = document.getElementById('canvas');
+	this.theCanvas = document.getElementById('photograb-canvas');
 	this.context = this.theCanvas.getContext('2d');
 	console.log("Got context");
 	console.log(screen.width, screen.height);
-	this.theCanvas.width = screen.width;
-	this.theCanvas.height = screen.height;
-	this.context.fillStyle = "#FFFFFF";
-	this.context.fillRect(0, 0, this.theCanvas.width, this.theCanvas.height);
+	//this.theCanvas.width = screen.width;
+	//this.theCanvas.height = screen.height;
+	//this.context.fillStyle = "#FFFFFF";
+	//this.context.fillRect(0, 0, this.theCanvas.width, this.theCanvas.height);
 	
 	this.theCanvas.addEventListener("mousemove", onSampMouseMove, false);
 	this.theCanvas.addEventListener("click", onSampMouseClick, false);
@@ -96,43 +97,41 @@ function photograb() {
 		}
 	}
 	
-	function doSomething(){
-		console.log("what would you like me to do?");
-		console.log( $("#pic").val());
-	}
-	
-	function imageLoaded() {
-	
-		console.log("imageLoaded");
-	
-		// Should we maybe resize based on the image size?
-		console.log(theApp.img.width, theApp.img.height);
-		console.log(theApp);
-	
-		theApp.context.fillStyle = "#000000";
-		theApp.context.fillRect(0, 0, theApp.theCanvas.width, theApp.theCanvas.height);
-		theApp.context.drawImage(theApp.img, 0, 0);
-		
-	}
+
 }
 
+function photograb_imageLoaded() {
+
+	console.log("imageLoaded");
+
+	// Should we maybe resize based on the image size?
+	console.log(photograb_img.width, photograb_img.height);
+
+	// Need to do something hear to clear the canvas to transparent...
+	theApp.context.fillStyle = "rgba(0, 0, 0, 1)";
+	theApp.context.fillRect(0, 0, theApp.theCanvas.width, theApp.theCanvas.width);
+	theApp.context.fillStyle = "rgba(255, 255, 255, 0)";
+	theApp.context.fillRect(0, 0, theApp.theCanvas.width, theApp.theCanvas.width);
+	theApp.context.drawImage(photograb_img, 0, 0);
+	
+}
+	
 function handlefiles(tf){
 	console.log("handlefiles got ", tf.length, " files");	
 
-	theApp.img = new Image();
-	theApp.img.name = "thingy";
-	theApp.img.classList.add("obj");
-	theApp.img.file = tf[0];
-	$(theApp.img).load(theApp.imageLoaded);
-	//theApp.img.addEventListener('load', theApp.imageLoaded , false);
-	console.log("grabbing ", theApp.img.file);
+	photograb_img = new Image();
+	photograb_img.name = "thingy";
+	photograb_img.classList.add("obj");
+	photograb_img.file = tf[0];
+	$(photograb_img).load(photograb_imageLoaded);
+	console.log("grabbing ", photograb_img.file);
 	 
 	var reader = new FileReader();
-	reader.onload = (function(aImg) { return function(e) { aImg.src = e.target.result; }; })(theApp.img);
+	reader.onload = (function(aImg) { return function(e) { aImg.src = e.target.result; }; })(photograb_img);
 	reader.readAsDataURL(tf[0]);
 	while (reader.readyState == false) {
 		var x = 1;
 	}
-	console.log(theApp.img);
+	console.log(photograb_img);
 	return;
 }

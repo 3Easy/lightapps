a7a66badadb6a711dd56783636e780cd2e6403b2
mpesce <mpesce@gmail.com>
2012-11-25 11:02:45 +1100

    Small css fixes in Sentimental, and added the Candle and Flame apps

---
+++ apps/flame/flame.js
@@ -0,0 +1,32 @@
+// candle.js - Run the candle animation
+//
+
+// Every app has an object
+function flame() {
+
+	this.appStart = appStart;
+	this.appQuit = appQuit;
+	this.running = false;
+	this.startbutton = flame_startbutton;
+	this.stopbutton = flame_stopbutton;
+	
+	function appStart() {
+		console.log("flame.appStart");
+	}
+	
+	function appQuit() {
+		console.log("flame.appQuit");
+	}
+}
+
+function flame_startbutton() {
+	console.log("flame_startbutton");
+	currentLight.sendcmd("flame-start.sh");
+	theApp.running = true;
+}
+
+function flame_stopbutton() {
+	console.log("flame_stopbutton");
+	currentLight.sendcmd("flame-quit.sh");
+	theApp.running = false;
+}
